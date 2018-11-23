407bc6d3ac8dece908f6bb39454d83d116c908b0
Oomkar Pandit <45208579+oomkarpanditstudent@users.noreply.github.com>
2018-11-23 19:11:07 +0000

    Feature1 (#1)
    * "Added a test java file for testing integration with intellij"
    
    * "Feature 1 edited and updated"

---
+++ Feature1File.java
@@ -0,0 +1,7 @@
+public class Feature1File {
+    Feature1File(){
+
+        System.out.println("Testing Adding File to Feature Branch");
+        System.out.println("Added a file to new feature");
+    }
+}
